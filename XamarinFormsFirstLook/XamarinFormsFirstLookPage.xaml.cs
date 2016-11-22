b0c7cb2b22fe431088ff687e52f95ce0efd9fd45
Shahab <tausiq19@gmail.com>
2016-11-22 08:06:48 +0600

    adds action sample

--- XamarinFormsFirstLook/XamarinFormsFirstLookPage.xaml.cs
+++ XamarinFormsFirstLook/XamarinFormsFirstLookPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace XamarinFormsFirstLook
 {
@@ -7,6 +8,11 @@ namespace XamarinFormsFirstLook
 		public XamarinFormsFirstLookPage()
 		{
 			InitializeComponent();
+		}
+
+		private void Submit_Clicked(object sender, EventArgs e)
+		{
+			DisplayAlert("Greetings!", "Hello from Xamarin.Forms!", "OK");
 		}
 	}
 }
